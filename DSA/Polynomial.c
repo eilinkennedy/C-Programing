805dd13582779f069ce88d29f4a4629d4d1185d9
eilinkennedy <166118422+eilinkennedy@users.noreply.github.com>
2024-09-06 21:16:43 +0530

    Create Polynomial.c

---
+++ DSA/Polynomial.c
@@ -0,0 +1,28 @@
+
+#include<stdio.h>
+struct polynomial{
+int coeff;
+int expo;
+};
+int main(){
+struct polynomial x[15];
+int term,i;
+printf("\nenter the no of terms:");
+scanf("%d",&term);
+printf("enter the coefficent term and exponent in descending order:");
+for(i=0;i<term;i++)
+{
+printf(" \n coeffecient:");
+scanf("%d",&x[i].coeff);
+printf("\n exponent:");
+scanf("%d",&x[i].expo);}
+printf("the polynomial is:");
+for(i=0;i<term;i++){
+printf("%dx^%d",x[i].coeff,x[i].expo);
+
+if(i<term-1)
+printf("+");
+}
+return 0;
+}
+Show quoted text
